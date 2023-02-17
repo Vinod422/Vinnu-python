9e4fb9ed0d60b0098d473b2b827717d5282e6eba
Vinnu <vinodkumarnvk777@gmail.com>
2023-02-17 18:05:48 +0530

    my first commit

---
+++ add.java
@@ -0,0 +1,15 @@
+//Addition of 2 numbers
+import java.io.*;
+class Addition
+{
+  public static void main(String args[])
+{
+    int a=200,b=400; // for storing 2 values
+    System.out.println("2 values are");
+    System.out.println("1st value =" +a);  //concatenation
+    System.out.println("2nd value =" +b);
+   //calculate add
+   int c=a+b;
+    System.out.println("addition="+c);
+}
+}
